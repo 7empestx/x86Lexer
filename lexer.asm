4f93bae5fb8f73abe3f83372dcdd3c6ff14471fc
GrantStarkman <starkg2@unlv.nevada.edu>
2023-05-28 12:42:15 -0600

    readChar impl and next token start

--- lexer.asm
+++ lexer.asm
@@ -3,6 +3,7 @@ section .data
     NULL    equ     0   ; end of string
     STDOUT  equ     1   ; standard output
     STDIN   equ     0   ; standard input
+    EOF     equ     -1  ; end of file
 
     SYS_write   equ 1   ; write
     SYS_read    equ 0   ; read
@@ -17,7 +18,14 @@ section .text
 _start:
     
     mov rdi, input  
-    call printString 
+    call printString
+
+    mov rdi, input
+    mov rsi, 0
+    mov rdx, 0
+    mov rcx, 0
+    mov r8, 0
+    call readChar
 
     ; Exit the program
     mov eax, 60            ; sys_exit system call
@@ -26,27 +34,36 @@ _start:
 
 global TestNextToken
 TestNextToken:
-
     ret
 
+; Arguments: 
+; input:        string  (rdi)
+; position:     int     (rsi) current position in input (points to current char)
+; readPosition: int     (rdx) current read position in input (after current char)
+; ch:           char    (rcx) current char under examination
+; inputLength:  int     (r8)  length of input
 global readChar
 readChar:
     
-    ; Check if the readPosition is greater than or equal to the length of the
-    ; input
-    cmp rsi, rdi
-    jge readCharDone
+    ; if l.readPosition >= len(l.input) { l.ch = 0 }
+    cmp rdx, r8
+    jge readInputDone
 
-    ; Not greater than or equal to the length of the input, set the current
-    ; char under examination to null
+    ; else { l.ch = l.input[l.readPosition] }
+    mov rcx, 0
+    mov cl, byte[rdi + rdx]
+    jmp readCharDone
+
+    readInputDone:
+        ; l.ch = 0
+        mov rcx, 0
+    
     readCharDone:
-    mov byte[rcx], NULL
-    jmp setNextChar
-
-    ; Set the current char under examination to the next char in the input
-    setNextChar:
-    mov byte[rcx], byte[rsi]
-    inc rsi
+        ; l.position = l.readPosition
+        mov rsi, rdx
+        ; l.readPosition += 1
+        inc rdx
+    ret
 
 global NextToken
 NextToken:
@@ -97,6 +114,17 @@ NextToken:
     cmp byte[rsi], EOF
     je  null
 
+    equal:
+    semicolon:
+    leftParenthesis:
+    rightParenthesis:
+    comma:
+    plus:
+    leftBrace:
+    rightBrace:
+    null:
+        ret
+
 
 
 
@@ -132,3 +160,19 @@ printString:
     printDone:
         ret
 
+; Arguments:
+; 1) address(rdi), string
+global getStringLength
+getStringLength:
+    ; Count chars in string
+    mov rbx, rdi
+    mov rdx, 0
+    
+    myStrCountLoop:
+        cmp byte[rbx], NULL
+        je strCountDone
+        inc rdx
+        inc rbx
+        jmp myStrCountLoop 
+    myStrCountDone:
+        ret
